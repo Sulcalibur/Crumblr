76e6f71b43e2e229f3412e52fe297ce389bf719f
Suleiman Leadbitter <suleiman.leadbitter@gmail.com>
2015-04-30 18:04:33 +0100

    Added the ability to Post linked images

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150429204651) do
+ActiveRecord::Schema.define(version: 20150430164404) do
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
     t.text     "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "kind"
   end
 
 end
