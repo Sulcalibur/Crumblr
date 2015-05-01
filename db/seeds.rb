862db7c4afdfd4463f1a707b9ea2a61778022703
Suleiman Leadbitter <suleiman.leadbitter@gmail.com>
2015-05-01 14:51:49 +0100

    Added seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+#Cheesecake Post
+post1 = Post.new
+post1.title = "Epic Cheesecake"
+post1.kind = "image"
+post1.content = "http://www.villageinn.com/i/pies/profile/cheesecake_main1.jpg"
+post1.save
+
+2.times do
+	post1.hearts.create
+end
+
+post2 = Post.new(title: "Ovenly", kind: "text")
+post2.content = "Tal's favourite bakery in Brooklyn is <%= link_to 'Ovenly', 'http://oven.ly' %>"
+post2.save
+
+5.times { post.hearts.create }
+
+
